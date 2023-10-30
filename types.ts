e26945114e8a6b2c1afd42cbbdf1c54af2d94295
WavyBoyJodii <najeecasey@gmail.com>
2023-10-30 17:45:15 -0400

    create type for posts from api

---
+++ types.ts
@@ -0,0 +1,11 @@
+export interface Post {
+  tags: string[];
+  _id: string;
+  title: string;
+  art: string;
+  mediaUrl: string;
+  content: string;
+  blogger: string;
+  __v: number;
+  date_created: string;
+}
